b487491a4eef4185e28cea92a300cebbdfc8d21a
Lazir Pascual <lazir.pascual@shopify.com>
2023-03-14 17:12:08 -0400

    Complete Numbers and Strings exercise

---
+++ Ruby/crowdfund/crowdfund.rb
@@ -0,0 +1,9 @@
+project1 = "ABC"
+funding1 = 1000
+project2 = "LMN"
+funding2 = 1000
+project3 = "XYZ"
+funding3 = 1000
+
+puts "Project #{project1} has #{funding1} in funding."
+puts "Projects: \n\tProject #{project1}\n\tProject #{project2}\n\tProject #{project3}\n"
--- Ruby/studio_game/studio_game.rb
+++ Ruby/studio_game/studio_game.rb
@@ -1,5 +1,9 @@
-greeting = "Welcome!"
-3.times do
-  puts greeting.upcase
-end
-puts Time.now
+name1 = "larry"
+health1 = 60
+name2 = "curly"
+health2 = 60
+name3 = "moe"
+health3 = 60
+
+puts "#{name1}'s health is #{health1/9.0}"
+puts "Players: \n\t#{name1}\n\t#{name2}\n\t#{name3}\n"
